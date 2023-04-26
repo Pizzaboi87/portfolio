74da13fd261bcc1cd4521b9d174d45c44503350a
Peter Weiser <peterweiser87@gmail.com>
2023-04-26 22:36:36 +0300

    added new project

--- src/components/main/links/Links.jsx
+++ src/components/main/links/Links.jsx
@@ -1,61 +1,75 @@
-import Modal from '../../modal/Modal'
-import '../portfolio/portfolio.css'
-import { useState } from 'react'
+import Modal from "../../modal/Modal";
+import "../portfolio/portfolio.css";
+import { useState } from "react";
+import YouTube from "react-youtube";
 
 const Links = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalOpen = () => {
+    setIsOpen(true);
+    props.setOverlay("opened");
+  };
+  const modalClose = () => {
+    setIsOpen(false);
+    props.setOverlay("closed");
+  };
 
-    const [isOpen, setIsOpen] = useState(false)
-    const modalOpen = () => {
-        setIsOpen(true)
-        props.setOverlay('opened')
-    }
-    const modalClose= () => {
-        setIsOpen(false)
-        props.setOverlay('closed')
-    }
+  const opts = {
+    height: "390",
+    width: "176",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-    return(
-        <div className='gallerydiv'>
-            <figure onClick={modalOpen}>
-                <figcaption className={props.new ? 'new' : null}>{props.title}</figcaption>
-                <img src={props.img_cover} alt={props.alt} />
-            </figure>
+  return (
+    <div className="gallerydiv">
+      <figure onClick={modalOpen}>
+        <figcaption className={props.new ? "new" : null}>
+          {props.title}
+        </figcaption>
+        <img src={props.img_cover} alt={props.alt} />
+      </figure>
 
-            <Modal open={isOpen}>
-                <i className='close fa-solid fa-circle-xmark' onClick={modalClose}></i>
-                <h1>{props.title}</h1>
-                <div className='details--pics'>
-                    {props.img_1 && <img src={props.img_1} alt={props.alt} />}
-                    {props.img_2 && <img src={props.img_2} alt={props.alt} />}
-                    {props.img_3 && <img src={props.img_3} alt={props.alt} />}
-                </div>
-                <div className='details--text'>
-                    <h2>Project details</h2>
-                    <p>{props.details}</p>
-                    <div className='details--link'>
-                        <a 
-                            href={props.github} 
-                            target='_blank' 
-                            rel='noreferrer' 
-                            className='icon'>
-                                <i className='fa-brands fa-square-github link'></i>
-                                <h4>GitHub Code</h4>
-                        </a>
-                        <a 
-                            href={props.link} 
-                            target='_blank' 
-                            rel='noreferrer' 
-                            className='icon'>
-                                <i className='fa-solid fa-link link'></i>
-                                <h4>Visit Website</h4>
-                        </a>
-                    </div>
-                </div>
-                <button onClick={modalClose} className='modalbutton'>Close</button>
-            </Modal>
+      <Modal open={isOpen}>
+        <i className="close fa-solid fa-circle-xmark" onClick={modalClose}></i>
+        <h1>{props.title}</h1>
+        <div className="details--pics">
+          {props.youTube && <YouTube videoId={props.youTube} opts={opts} />}
+          {props.img_1 && <img src={props.img_1} alt={props.alt} />}
+          {props.img_2 && <img src={props.img_2} alt={props.alt} />}
+          {props.img_3 && <img src={props.img_3} alt={props.alt} />}
         </div>
-        
-    )
-}
+        <div className="details--text">
+          <h2>Project details</h2>
+          <p>{props.details}</p>
+          <div className="details--link">
+            <a
+              href={props.github}
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <i className="fa-brands fa-square-github link"></i>
+              <h4>GitHub Code</h4>
+            </a>
+            <a
+              href={props.link}
+              target="_blank"
+              rel="noreferrer"
+              className="icon"
+            >
+              <i className="fa-solid fa-link link"></i>
+              <h4>{props.youTube ? "Download App" : "Visit Website"}</h4>
+            </a>
+          </div>
+        </div>
+        <button onClick={modalClose} className="modalbutton">
+          Close
+        </button>
+      </Modal>
+    </div>
+  );
+};
 
 export default Links;
--- src/components/main/portfolio/Portfolio.jsx
+++ src/components/main/portfolio/Portfolio.jsx
@@ -1,73 +1,75 @@
-import batman_sign from '../../../assets/batman.webp'
-import joker_figure from '../../../assets/joker.webp'
-import { useEffect, useRef,useState } from 'react'
-import wave3 from '../../../assets/wave3.svg'
-import Links from '../links/Links'
-import data from '../links/data'
-import './portfolio.css'
+import batman_sign from "../../../assets/batman.webp";
+import joker_figure from "../../../assets/joker.webp";
+import { useEffect, useRef, useState } from "react";
+import wave3 from "../../../assets/wave3.svg";
+import Links from "../links/Links";
+import data from "../links/data";
+import "./portfolio.css";
 
 const Portfolio = () => {
+  const [overlay, setOverlay] = useState("initial");
 
-    const [overlay, setOverlay] = useState('initial')
+  const callBatman = useRef(null);
+  const joker = useRef(null);
+  const batman = useRef(null);
 
-    const callBatman = useRef(null)
-    const joker = useRef(null)
-    const batman = useRef(null)
+  useEffect(() => {
+    const jokerEvent = () => {
+      joker.current.classList.add("hideJoker");
+      setTimeout(batEvent, 500);
+    };
 
-    useEffect(() => {
-        const jokerEvent = () => {
-            joker.current.classList.add('hideJoker')
-            setTimeout(batEvent, 500)
-        }
+    const hideBatman = (e) => {
+      if (e.target == batman.current && e.target !== callBatman.current) {
+        batman.current.style.display = "none";
+        callBatman.current.removeEventListener("click", jokerEvent);
+      }
+    };
 
-        const hideBatman = (e) => {
-            if (e.target == batman.current && e.target !== callBatman.current) {
-                batman.current.style.display = "none"
-                callBatman.current.removeEventListener('click', jokerEvent)
-            }
-        }
+    const batEvent = () => {
+      batman.current.style.display = "inline";
+      window.addEventListener("click", hideBatman);
+    };
 
-        const batEvent = () => {
-            batman.current.style.display = 'inline'
-            window.addEventListener('click', hideBatman)
-        }
+    callBatman.current.addEventListener("click", jokerEvent);
+    return () => window.removeEventListener("click", hideBatman);
+  }, []);
 
-        callBatman.current.addEventListener('click', jokerEvent)
-        return () => window.removeEventListener('click', hideBatman)
-    }, [])
-
-    const links = data.map(data => {
-        return(
-          <Links
-            key={data.id}
-            link={data.link}
-            github={data.github}
-            img_cover={data.img_cover}
-            img_1={data.img_1}
-            img_2={data.img_2}
-            img_3={data.img_3}
-            alt={data.alt}
-            title={data.title}
-            details={data.details}
-            new={data.new}
-            setOverlay={setOverlay}
-          />
-        )
-      })
-
+  const links = data.map((data) => {
     return (
-        <section id='portfolio'>
-            <div className={overlay === 'closed' ? 'overlay closedmodal' : 'openedmodal'}></div>
-            <h1 className='title'>My Projects</h1>
-            <div className='gallery'>
-                {links}
-            </div>
-            <img src={batman_sign} ref={batman} className='batman' alt='batman' />
-            <img src={joker_figure} ref={joker} className='joker' alt='joker' />
-            <img src={wave3} className='wave jokerwave' alt='wave' />
-            <button ref={callBatman} className='button'>Push</button>
-        </section>
-    )
-}
+      <Links
+        key={data.id}
+        link={data.link}
+        github={data.github}
+        img_cover={data.img_cover}
+        youTube={data.youTube}
+        img_1={data.img_1}
+        img_2={data.img_2}
+        img_3={data.img_3}
+        alt={data.alt}
+        title={data.title}
+        details={data.details}
+        new={data.new}
+        setOverlay={setOverlay}
+      />
+    );
+  });
+
+  return (
+    <section id="portfolio">
+      <div
+        className={overlay === "closed" ? "overlay closedmodal" : "openedmodal"}
+      ></div>
+      <h1 className="title">My Projects</h1>
+      <div className="gallery">{links}</div>
+      <img src={batman_sign} ref={batman} className="batman" alt="batman" />
+      <img src={joker_figure} ref={joker} className="joker" alt="joker" />
+      <img src={wave3} className="wave jokerwave" alt="wave" />
+      <button ref={callBatman} className="button">
+        Push
+      </button>
+    </section>
+  );
+};
 
 export default Portfolio;
